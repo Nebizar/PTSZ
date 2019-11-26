2623093f32cb11eb41544c796e9d0cb42b0f8a82
mwolicki <mikolajw1997@gmail.com>
2019-11-26 01:02:06 +0100

    fix alg

--- algorithms/132344/a1.py
+++ algorithms/132344/a1.py
@@ -7,18 +7,27 @@ machineNumbers = 4
 
 class Task(object):
     def __init__(self, p, r, d, id):
-        self.r = r
         self.p = p
+        self.r = r
         self.d = d
         self.id = id
 
 
 class Machine(object):
-    def __init__(self, id, isOcuppied, shouldEnd):
+    def __init__(self, id):
         self.id = id
-        self.isOcuppied = isOcuppied
         self.tasks = []
-        self.shouldEnd = shouldEnd
+        self.taskEnd = 0
+        self.delay = 0
+    def addTask(self,task):
+        self.tasks.append(task)
+        if task.r > self.taskEnd:
+            self.taskEnd = task.r
+        self.taskEnd += task.p
+        if self.taskEnd > task.d:
+            self.delay += (self.taskEnd - task.d)
+
+
 
 def main():
     file = open("instances/" + sys.argv[1] + "/" + sys.argv[2] + ".txt", "r")
@@ -28,45 +37,29 @@ def main():
     for number in range(0, int(number_of_rows)):
         line = file.readline()
         line = line.strip().split(' ')
-        tasksList.append(Task(int(line[0]), line[1], line[2], number))
+        tasksList.append(Task(int(line[0]), int(line[1]), int(line[2]), number))
     file.close()
     # Calculated Value from file
-    calculatedE = 0
-    timeLine = 0
+    tardiness = 0
     machinesList = []
     # Create machines
     for id in range(0, machineNumbers):
-        machinesList.append(Machine(id, False, 0))
+        machinesList.append(Machine(id))
 
     # Sort tasks based on d
     tasksList = sorted(tasksList, key=lambda x: int(x.d))
 
-    # Continue if there is any task to put on machine
-    while len(tasksList) != 0:
-        # Check every machine
-        for machine in machinesList:
-            if(machine.isOcuppied): 
-                if(machine.shouldEnd <= timeLine):
-                    machine.isOcuppied = False
+    # start
+    for task in tasksList:
+        machine_curr = next(machine for machine in machinesList if machine.taskEnd == min(m.taskEnd for m in machinesList))
+        machine_curr.addTask(task)
 
-            if(not(machine.isOcuppied)):
-                # Get task with lowest 'd' value
-                testedTask = tasksList[0]
-                if(timeLine >= int(testedTask.r)):
-                    # Start task on machine
-                    machine.isOcuppied = True
-                    machine.tasks.append(testedTask)
-                    machine.shouldEnd = timeLine + int(testedTask.p)
-                    calculatedE = calculatedE + max(0, machine.shouldEnd - int(testedTask.d))
-                    tasksList.pop(0)
-                    if (len(tasksList) == 0):
-                        break
-        timeLine = timeLine + 1
-    
+    tardiness = sum(machine.delay for machine in machinesList)
+
     if not os.path.exists("results/" + "/132344/a1/" + sys.argv[1] + "/"):
         os.makedirs("results/" + "/132344/a1/" + sys.argv[1] + "/")
     file = open("results/" + "/132344/a1/" + sys.argv[1] + "/" + sys.argv[2] + ".txt", "w+")
-    file.write(str(calculatedE) + "\n")
+    file.write(str(tardiness) + "\n")
     for machine in machinesList:
         for task in machine.tasks:
             file.write(str(task.id + 1) + ' ')
