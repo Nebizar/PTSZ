232289967838d430d8fa5f6f3c39a532cafdcec9
Kacper <kacper1717@gmail.com>
2019-11-26 23:14:35 +0100

    XD

--- algorithms/132274/a1.py
+++ algorithms/132274/a1.py
@@ -45,63 +45,32 @@ def sortTasks(tasks, i):
 
 def orderTasks(tasks_d, tasks_r, i, times, taskOrder, ds):
     for y in range(0, int(i)):
-        single_tardiness = 0
         processor_no = times.index((min(times)))
         if times[processor_no] >= tasks_d[0][2]:
+            print(tasks_d[0][3])
             times[processor_no] = times[processor_no] + tasks_d[0][1]
             taskOrder[processor_no].append(str(tasks_d[0][0] + 1))
-            single_tardiness = times[processor_no] - tasks_d[0][3]
             del tasks_r[tasks_r.index(tasks_d[0])]
             del tasks_d[0]
         else:
-            if tasks_d[0][2] >= tasks_r[0][2]:  # jesli d moze sie zaczac przed r
-                times[processor_no] = tasks_d[0][2] + tasks_d[0][1]
+            if (times[processor_no] >= tasks_r[0][2]):
+                times[processor_no] = times[processor_no] + tasks_r[0][1]
+                taskOrder[processor_no].append(str(tasks_r[0][0] + 1))
+                del tasks_d[tasks_d.index(tasks_r[0])]
+                del tasks_r[0]
+            elif ((tasks_d[0][3] - tasks_d[0][2] + tasks_d[0][1]) > (tasks_r[0][3] - tasks_r[0][2] + tasks_r[0][1])):
+                times[processor_no] = tasks_r[0][2] + tasks_r[0][1] + times[processor_no]
+                taskOrder[processor_no].append(str(tasks_r[0][0] + 1))
+                del tasks_d[tasks_d.index(tasks_r[0])]
+                del tasks_r[0]
+            else:
+                times[processor_no] = tasks_d[0][2] + tasks_d[0][1] + times[processor_no]
                 taskOrder[processor_no].append(str(tasks_d[0][0] + 1))
-                single_tardiness = times[processor_no] - tasks_d[0][3]
                 del tasks_r[tasks_r.index(tasks_d[0])]
                 del tasks_d[0]
-            else:
-                if times[processor_no] >= tasks_d[0][2]:  # jesli r mozna juz od razu wykonywac
-                    if (tasks_d[0][1] + tasks_d[0][2]) < (times[processor_no] + tasks_r[0][1]):
-                        times[processor_no] = tasks_d[0][2] + tasks_d[0][1]
-                        taskOrder[processor_no].append(str(tasks_d[0][0] + 1))
-                        single_tardiness = times[processor_no] - tasks_d[0][3]
-                        del tasks_r[tasks_r.index(tasks_d[0])]
-                        del tasks_d[0]
-                    else:
-                        times[processor_no] = tasks_r[0][2] + tasks_r[0][1]
-                        taskOrder[processor_no].append(str(tasks_r[0][0] + 1))
-                        single_tardiness = times[processor_no] - tasks_r[0][3]
-                        del tasks_d[tasks_d.index(tasks_r[0])]
-                        del tasks_r[0]
-                else:  # jesli r nie mozna od razu wykonywac
-                    if (tasks_d[0][1] + tasks_d[0][2]) < (tasks_r[0][1] + tasks_r[0][2]):
-                        times[processor_no] = tasks_d[0][2] + tasks_d[0][1]
-                        taskOrder[processor_no].append(str(tasks_d[0][0] + 1))
-                        single_tardiness = times[processor_no] - tasks_d[0][3]
-                        del tasks_r[tasks_r.index(tasks_d[0])]
-                        del tasks_d[0]
-                    else:
-                        times[processor_no] = tasks_r[0][2] + tasks_r[0][1]
-                        taskOrder[processor_no].append(str(tasks_r[0][0] + 1))
-                        single_tardiness = times[processor_no] - tasks_r[0][3]
-                        del tasks_d[tasks_d.index(tasks_r[0])]
-                        del tasks_r[0]
-        if single_tardiness > 0:
-            ds[processor_no] = ds[processor_no] + single_tardiness
 
 
-# def saveToFile(i):
-#     file = open(str(i) + "out.txt", "w+")
-#     file.write(str(sum(ds)) + "\n")
-#     for x in range(0, 4):
-#         for y in range(0, len(taskOrder[x])):
-#             file.write(str(taskOrder[x][y]) + " ")
-#         file.write("\n")
-
-
-def save_result(algorithm_author, algorithm, instance_author, instance_size, taskOrder, ds):
-    print("zapisuje")
+def save_result(algorithm_author, algorithm, instance_author, instance_size, taskOrder, ds, tardiness):
     filename = os.path.join("results",
                             str(algorithm_author),
                             str(algorithm),
@@ -109,15 +78,26 @@ def save_result(algorithm_author, algorithm, instance_author, instance_size, tas
                             "{}.txt".format(instance_size))
     os.makedirs(os.path.dirname(filename), exist_ok=True)
     with open(filename, "w") as f:
-        f.write(str(sum(ds)) + "\n")
+        f.write(str(tardiness) + "\n")
         for x in range(0, 4):
             for y in range(0, len(taskOrder[x])):
                 f.write(str(taskOrder[x][y]) + " ")
             f.write("\n")
 
 
+def checkTardinessForOneProcessor(order, tasks):
+    tardiness = 0
+    start = 0
+    for t in order:
+        t = int(t) - 1
+        start = max(tasks[1][t], start)
+        end = start + tasks[0][t]
+        tardiness = tardiness + max(0, end - tasks[2][t])
+        start = end
+    return tardiness
+
+
 def main():
-    print("zaczynam")
     algorithm_author = "132274"
     algorithm = "a1"
     instance_author = sys.argv[1]
@@ -135,29 +115,14 @@ def main():
         times.append(0)
         ds.append(0)
     orderTasks(tasks_d, tasks_r, instance_size, times, taskOrder, ds)
-
+    tardiness = 0
+    for i in range(0, 4):
+        tardiness += checkTardinessForOneProcessor(taskOrder[i], tasks)
     save_result(algorithm_author,
                 algorithm,
                 instance_author,
-                instance_size, taskOrder, ds)
+                instance_size, taskOrder, ds, tardiness)
 
 
 if __name__ == "__main__":
     main()
-
-# for z in range(50, 550, 50):
-#     instanceSize = z
-#     # instanceSize = z
-#     tasks, noTasks = readTasks(instanceSize)
-#     tasks_d, tasks_r = sortTasks(tasks, instanceSize)
-#     times = []
-#     ds = []
-#     taskOrder = [[], [], [], []]
-#     for x in range(0, 4):
-#         times.append(0)
-#         ds.append(0)
-#     start_time = time.time()
-#     orderTasks(tasks_d, tasks_r, instanceSize, times, taskOrder, ds)
-#     elapsed_time = time.time() - start_time
-#     print(elapsed_time)
-#     saveToFile(instanceSize)
