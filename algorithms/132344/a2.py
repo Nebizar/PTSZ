7660669bc35b178173505606e00e82c7844004b6
Mikolaj Wolicki <wolicki.mikolaj@gmail.com>
2020-01-08 03:43:26 +0100

    Fix a2

--- algorithms/132344/a2.py
+++ algorithms/132344/a2.py
@@ -3,6 +3,7 @@ import math
 import os
 import time
 import random
+from copy import deepcopy
 
 machineNumbers = 4
 
@@ -12,6 +13,9 @@ class Task(object):
         self.r = r
         self.d = d
         self.id = id
+        self.start = 0
+        self.end = 0
+        self.generatedDelay = 0
 
 
 class Machine(object):
@@ -20,13 +24,18 @@ class Machine(object):
         self.tasks = []
         self.taskEnd = 0
         self.delay = 0
-    def addTask(self,task):
-        self.tasks.append(task)
+
+    def addTask(self, task):
+        task.start = self.taskEnd
         if task.r > self.taskEnd:
             self.taskEnd = task.r
         self.taskEnd += task.p
+        task.end = self.taskEnd
         if self.taskEnd > task.d:
             self.delay += (self.taskEnd - task.d)
+            task.generatedDelay = (self.taskEnd - task.d)
+        self.tasks.append(task)
+
 
 class Solution(object):
     def __init__(self, tardiness, tasksList, machinesList):
@@ -34,10 +43,14 @@ class Solution(object):
         self.tasksList = tasksList
         self.machinesList = machinesList
 
+
 def swapArray(seq):
     idx = range(len(seq))
     i1, i2 = random.sample(idx, 2)
+    toSwap = next((task for task in seq if isGoodForSwap(task, seq[i1])), seq[i2])
+    i2 = seq.index(toSwap)
     seq[i1], seq[i2] = seq[i2], seq[i1]
+
 
 def calculateSolution(tasksList):
     tardiness = 0
@@ -55,18 +68,46 @@ def calculateSolution(tasksList):
 
 def generatePopulation(tasksList, numOfSwaps, populationSize):
     population = []
+    newList = None
     for _ in range(populationSize):
-        newList = tasksList
+        newList = deepcopy(tasksList)
         for _ in range(numOfSwaps):
             swapArray(newList)
         population.append(newList)
     return population
+
 
 def calculatePopulation(population):
     solutionsList = []
     for item in population:
         solutionsList.append(calculateSolution(item))
     return solutionsList
+
+def isGoodForSwap(new, old):
+    newTask = deepcopy(new)
+    oldTask = deepcopy(old)
+    newDelayOne = 0
+    newDelayTwo = 0
+    pom = oldTask.start
+    oldTask.start = newTask.start
+    newTask.start = pom
+
+    if(newTask.start < newTask.r):
+        newTask.start = newTask.r
+
+    if(oldTask.start < oldTask.r):
+        oldTask.start = oldTask.r
+
+    if(oldTask.start + oldTask.p > oldTask.d):
+        newDelayOne = (oldTask.start + oldTask.p) - oldTask.d
+
+    if(newTask.start + newTask.p > newTask.d):
+        newDelayTwo = (newTask.start + newTask.p) - newTask.d
+
+    if(newDelayOne + newDelayTwo <= newTask.generatedDelay + oldTask.generatedDelay):
+        return True
+    return False
+
 
 def main():
     file = open("instances/" + sys.argv[1] + "/" + sys.argv[2] + ".txt", "r")
@@ -83,29 +124,34 @@ def main():
     tasksList = sorted(tasksList, key=lambda x: int(x.d))
 
     # Magic below - parameters
-    populationSize = 1000
-    numOfGenerations = 10
+    populationSize = 10
+    numOfGenerations = 5
     numOfSwaps = int(len(tasksList) * 0.05)
+    maxTime = time.time() + 0.01 * number_of_rows
 
-    bestSolutionEver = None
+    bestSolutionEver = calculateSolution(deepcopy(tasksList))  # First best solution
+    bestSolutionFromPopulation = deepcopy(bestSolutionEver)
 
-    for generation in range(numOfGenerations):
-        print("Generation : ", generation)
+    # print("Entry best solution : ", bestSolutionFromPopulation.tardiness)
+
+    while(time.time() < maxTime and numOfGenerations > 0):
         population = []
         solutions = []
 
-        population = generatePopulation(tasksList, numOfSwaps, populationSize)
+        population = generatePopulation(deepcopy(bestSolutionFromPopulation.tasksList), numOfSwaps, populationSize)
         solutions = calculatePopulation(population)
 
         bestSolutionFromPopulation = min(solutions, key=lambda x: x.tardiness)
-        print("Best solution : ", bestSolutionFromPopulation.tardiness)
 
-        if(bestSolutionEver == None or bestSolutionEver.tardiness > bestSolutionFromPopulation.tardiness):
-            bestSolutionEver = bestSolutionFromPopulation
-            tasksList = bestSolutionEver.tasksList
+        # print("Best solution population : ",bestSolutionFromPopulation.tardiness)
+
+        if(bestSolutionEver.tardiness > bestSolutionFromPopulation.tardiness):
+            bestSolutionEver = deepcopy(bestSolutionFromPopulation)
+            
+        numOfGenerations+= -1
 
     # Output to file
-    print("Best solution ever: ", bestSolutionEver.tardiness)
+    # print("Best solution ever : ", bestSolutionEver.tardiness)
     if not os.path.exists("results/" + "/132344/a2/" + sys.argv[1] + "/"):
         os.makedirs("results/" + "/132344/a2/" + sys.argv[1] + "/")
     file = open("results/" + "/132344/a2/" + sys.argv[1] + "/" + sys.argv[2] + ".txt", "w+")
@@ -116,8 +162,6 @@ def main():
         if(machine.id != 3):
             file.write('\n')
     file.close()
-
-
 
 if __name__ == '__main__':
     main()
